051f015e255435e6a2795e544ad26494fd949395
liciously <39293192+liciously@users.noreply.github.com>
2022-04-19 19:10:09 +0700

    Update local_settings.py

--- Hari-4/horizon/local_settings.py
+++ Hari-4/horizon/local_settings.py
@@ -3,14 +3,14 @@
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': 'controller:11211',
+        'LOCATION': '172.16.4.115:11211',
     },
 }
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
-OPENSTACK_HOST = "10.30.100.1"
-OPENSTACK_KEYSTONE_URL = "http://10.30.100.1:5000/v3"
+OPENSTACK_HOST = "172.16.4.115"
+OPENSTACK_KEYSTONE_URL = "http://172.16.4.115:5000/v3"
 
 TIME_ZONE = "Asia/Jakarta"
 
